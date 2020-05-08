c42425bb371321c9fb26dbfd866a4067bbb7ed5b
robin.son <robin.son@kakaocorp.com>
2020-05-08 23:24:30 +0900

    [optional] SoundCard에 USB setter 추가

--- src/main/java/com/company/objects/SoundCard.java
+++ src/main/java/com/company/objects/SoundCard.java
@@ -1,10 +1,14 @@
 package com.company.objects;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Optional;
 
 @Getter
 public class SoundCard {
+
+    @Setter
     private Optional<USB> usb;
+
 }
