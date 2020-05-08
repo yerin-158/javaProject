deb1adbdd7c66ed77dece24364b5dc5b6cd3518c
robin.son <robin.son@kakaocorp.com>
2020-05-08 22:33:15 +0900

    [optional] USB의 시리얼 넘버를 셋 할 수 있도록 클래스 수정

--- src/main/java/com/company/objects/USB.java
+++ src/main/java/com/company/objects/USB.java
@@ -1,8 +1,14 @@
 package com.company.objects;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class USB {
     private String serialNumber;
+
+    public USB(String serialNumber){
+        this.serialNumber = serialNumber;
+    }
 }
