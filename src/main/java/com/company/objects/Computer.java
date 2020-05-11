54cc024ef2ad3955ebf03ec8a7c3950653c12cb6
robin.son <robin.son@kakaocorp.com>
2020-05-11 15:02:14 +0900

    [optional] 빌더 추가

--- src/main/java/com/company/objects/Computer.java
+++ src/main/java/com/company/objects/Computer.java
@@ -1,10 +1,23 @@
 package com.company.objects;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Optional;
 
 @Getter
 public class Computer {
+
+    @Setter
     private Optional<SoundCard> soundCard;
+
+    @Builder
+    public Computer (SoundCard soundCard){
+        this.soundCard = Optional.ofNullable(soundCard);
+    }
+
+    public Optional<Computer> toOptional () {
+        return Optional.ofNullable(this);
+    }
 }
--- src/main/java/com/company/objects/SoundCard.java
+++ src/main/java/com/company/objects/SoundCard.java
@@ -1,14 +1,21 @@
 package com.company.objects;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Optional;
 
 @Getter
+@NoArgsConstructor
 public class SoundCard {
 
     @Setter
     private Optional<USB> usb;
 
+    @Builder
+    public SoundCard (USB usb){
+        this.usb = Optional.ofNullable(usb);
+    }
 }
